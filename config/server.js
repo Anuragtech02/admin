a397c7877c896c29860118d7edd28096479022f4
Anurag Pal <apal895@gmail.com>
2026-01-29 20:17:19 +0530

    fix: correct cron time to 9:15 AM CST (15:15 UTC)

--- config/server.js
+++ config/server.js
@@ -8,8 +8,8 @@ module.exports = ({ env }) => ({
   cron: {
     enabled: true,
     tasks: {
-      // Run daily at 9:15 AM Central (14:15 UTC)
-      "15 14 * * *": async ({ strapi }) => {
+      // Run daily at 9:15 AM CST (15:15 UTC)
+      "15 15 * * *": async ({ strapi }) => {
         console.log("Starting daily user certificate expiry check...");
         await strapi
           .service("api::user-certificate.user-certificate")
